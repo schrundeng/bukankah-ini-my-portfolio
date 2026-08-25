667fe863452a251a69bc13c75927cd6ded1ceab5
schrundeng <yangticamilan@gmail.com>
2026-08-25 18:45:36 +0700

    fix: navbar sticky on top wth tinted bg blur

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="min-h-screen bg-primary">
-    <div class="h-screen min-h-[650px] flex flex-col">
+    <div class="sticky top-0 z-50 backdrop-blur-md bg-[#0E0E0E]/80">
       <Header />
+    </div>
+    <div class="h-screen min-h-[650px] flex flex-col">
       <HeroSection />
     </div>
     <AboutSection />
